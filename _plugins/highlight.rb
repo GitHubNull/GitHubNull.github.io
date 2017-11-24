da7a8cad0e2c28de882a3951d382006e673f0880
null <641570479@qq.com>
2017-11-25 02:24:48 +0800

    update

--- _plugins/highlight.rb
+++ _plugins/highlight.rb
@@ -1,6 +1,6 @@
 module Jekyll
   module Tags
-    class CustomeHighlightBlock < Liquid::Block #因为jekyll的lib里有相同类的定义，这里修改为其他类名
+    class HighlightBlock < Liquid::Block
       include Liquid::StandardFilters
 
       # The regular expression syntax checker. Start with the language specifier.
@@ -27,7 +27,9 @@ module Jekyll
               @options[key.to_sym] = value || true
             end
           end
-          @options[:lineanchors] = true  # 要修改的地方
+          #@options[:linenos] = "inline" if @options.key?(:linenos) and @options[:linenos] == true
+          #@options[:linenos] = "inline" 
+          @options[:lineanchors] = true
         else
           raise SyntaxError.new <<-eos
 Syntax Error in tag 'highlight' while parsing the following markup:
@@ -39,9 +41,87 @@ eos
         end
       end
 
-      
+      def render(context)
+        prefix = context["highlighter_prefix"] || ""
+        suffix = context["highlighter_suffix"] || ""
+        code = super.to_s.strip
+
+        is_safe = !!context.registers[:site].safe
+
+        output =
+          case context.registers[:site].highlighter
+            when 'pygments'
+              render_pygments(code, is_safe)
+            when 'rouge'
+              render_rouge(code)
+            else
+              render_codehighlighter(code)
+            end
+
+        rendered_output = add_code_tag(output)
+        prefix + rendered_output + suffix
+      end
+
+      def sanitized_opts(opts, is_safe)
+        if is_safe
+          Hash[[
+            [:startinline, opts.fetch(:startinline, nil)],
+            [:hl_linenos,  opts.fetch(:hl_linenos, nil)],
+            [:linenos,     opts.fetch(:linenos, nil)],
+            [:encoding,    opts.fetch(:encoding, 'utf-8')],
+            [:cssclass,    opts.fetch(:cssclass, nil)]
+          ].reject {|f| f.last.nil? }]
+        else
+          opts
+        end
+      end
+
+      def render_pygments(code, is_safe)
+        require 'pygments'
+
+        @options[:encoding] = 'utf-8'
+
+        highlighted_code = Pygments.highlight(
+          code,
+          :lexer   => @lang,
+          :options => sanitized_opts(@options, is_safe)
+        )
+
+        if highlighted_code.nil?
+          Jekyll.logger.error "There was an error highlighting your code:"
+          puts
+          Jekyll.logger.error code
+          puts
+          Jekyll.logger.error "While attempting to convert the above code, Pygments.rb" +
+            " returned an unacceptable value."
+          Jekyll.logger.error "This is usually a timeout problem solved by running `jekyll build` again."
+          raise ArgumentError.new("Pygments.rb returned an unacceptable value when attempting to highlight some code.")
+        end
+
+        highlighted_code
+      end
+
+      def render_rouge(code)
+        require 'rouge'
+        formatter = Rouge::Formatters::HTML.new(line_numbers: @options[:linenos], wrap: false)
+        lexer = Rouge::Lexer.find_fancy(@lang, code) || Rouge::Lexers::PlainText
+        code = formatter.format(lexer.lex(code))
+        "<div class=\"highlight\"><pre>#{code}</pre></div>"
+      end
+
+      def render_codehighlighter(code)
+        "<div class=\"highlight\"><pre>#{h(code).strip}</pre></div>"
+      end
+
+      def add_code_tag(code)
+        # Add nested <code> tags to code blocks
+        code = code.sub(/<pre>\n*/,'<pre><code class="language-' + @lang.to_s.gsub("+", "-") + '" data-lang="' + @lang.to_s + '">')
+        code = code.sub(/\n*<\/pre>/,"</code></pre>")
+        code.strip
+      end
+
     end
   end
 end
 
-Liquid::Template.register_tag('highlight', Jekyll::Tags::CustomeHighlightBlock) #因为jekyll的lib里有相同类的定义，这里修改为其他类名
+Liquid::Template.register_tag('highlight', Jekyll::Tags::HighlightBlock)
